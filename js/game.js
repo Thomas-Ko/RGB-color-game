7e42904cd26d333907938a86982483f8d951612f
Thomas-Ko <tomkolodzi@gmail.com>
2016-02-13 12:58:23 -0500

    Add winner class when winning color clicked / clean some code up

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 
 
 var correctColor = null;
+var gameOn = true;
 
 //creates an rgb property with random values, used for setting background colors of .color-square divs
 function setRGB() {
@@ -15,15 +16,18 @@ function setRGB() {
 var colorSquares = document.getElementsByClassName("color-square");
 
 //sets random background colors for each colorSquares
-for (i=0; i<colorSquares.length; i++) {
-	colorSquares[i].style.background= setRGB();
+
+function setRGBforAll() {
+	for (i=0; i<colorSquares.length; i++) {
+		colorSquares[i].style.background= setRGB();
+	}
 }
 
 //retrieves the span tag with id of correctColor, this is where the correct color will be printed
 var correctColorHTML = document.getElementById("correctColor"); 
 
 //sets correctColor to one of the colorSquares' background color and displays on the html page 
-function newCorrectColor() {
+function setCorrectColor() {
 	var i = Math.floor((Math.random() * 5) + 0);
 	correctColor = colorSquares[i].style.background;	
 	correctColorHTML.textContent = correctColor;
@@ -31,18 +35,32 @@ function newCorrectColor() {
 /*Creator's note: At first I thought of setting the correctColor first and then assigning that to one of
 the correctColors items, but I quickly realized that would not be an elegant solution */
 
-newCorrectColor();
-
 //checks to see if the item clicked corresponds to the correctColor variable;
 function checkColor() {
-	if (this.style.background===correctColor){
-		console.log("winner");
-	} else {
-		console.log("loser");
+	if (gameOn) {
+		if (this.style.background===correctColor){
+			console.log("winner");
+			this.classList.add("winner");	//adds .winner class which gives the div a nice glow
+			gameOn=false;	//turns game off
+		} else {
+			this.style.opacity="0.0"; //maybe not the most elegant solution to make div disappear
+			console.log("false");
+		}
 	}
 }
 
 //adds a click event to each item of colorSquares, when clicked it runs the checkColor function
-for (i=0; i<colorSquares.length; i++){
-	colorSquares[i].addEventListener("click",checkColor);
+function makeColorsClickable () {
+	for (i=0; i<colorSquares.length; i++){
+		colorSquares[i].addEventListener("click",checkColor);
+	}
 }
+
+function reset() {
+	gameOn=true;
+	setRGBforAll();
+	setCorrectColor();
+	makeColorsClickable();
+}
+
+reset();
