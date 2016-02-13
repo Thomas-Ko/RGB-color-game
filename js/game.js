dca227aff4625f072524d7491b7384c2ac7b4990
Thomas-Ko <tomkolodzi@gmail.com>
2016-02-13 11:48:00 -0500

    Create newCorrectColor function / Display that information in html file

--- js/game.js
+++ js/game.js
@@ -2,7 +2,8 @@
 
 var correctColor = null;
 
-function colorize() {
+//creates an rgb property with random values, used for setting background colors of .color-square divs
+function setRGB() {
 	function randomNumber() { return Math.floor((Math.random() * 255) + 0);}
 	var num1 = randomNumber();
 	var num2 = randomNumber();
@@ -10,8 +11,22 @@ function colorize() {
 	return "rgb(" + num1 + "," + num2 + "," + num3 + ")";
 }
 
+//retrieves divs with "color-square" classes
 var colorSquares = document.getElementsByClassName("color-square");
 
+//sets random background colors for each colorSquares
 for (i=0; i<colorSquares.length; i++) {
-	colorSquares[i].style.background= colorize();
+	colorSquares[i].style.background= setRGB();
 }
+
+//retrieves the span tag with id of correctColor, this is where the correct color will be printed
+var correctColorHTML = document.getElementById("correctColor"); 
+
+//sets correctColor to one of the colorSquares' background color and displays on the html page 
+function newCorrectColor() {
+	var i = Math.floor((Math.random() * 5) + 0);
+	correctColor = colorSquares[i].style.background;	
+	correctColorHTML.textContent = correctColor;
+}
+
+newCorrectColor();
